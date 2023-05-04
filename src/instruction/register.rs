436900590139963d8fbff68617b9aa076ca7904b
lemonadern <taishi.develop@gmail.com>
2023-05-05 04:51:00 +0900

    feat: implement register parser

--- src/instruction/register.rs
+++ src/instruction/register.rs
@@ -1,4 +1,4 @@
-use anyhow::{anyhow, Result};
+use anyhow::{anyhow, Ok, Result};
 
 use crate::binary::binary_string;
 
@@ -38,8 +38,38 @@ fn parse_register(input: &str) -> Result<Register> {
     }
 
     match input {
-        "$30" | "$fp" => Ok(Register::new(30, input)),
-        // TODO: Implement other registers
+        "$0" | "$zero" => Ok(Register::new(0, "zero")),
+        "$1" | "$at" => Ok(Register::new(1, "at")),
+        "$2" | "$v0" => Ok(Register::new(2, "v0")),
+        "$3" | "$v1" => Ok(Register::new(3, "v1")),
+        "$4" | "$a0" => Ok(Register::new(4, "a0")),
+        "$5" | "$a1" => Ok(Register::new(5, "a1")),
+        "$6" | "$a2" => Ok(Register::new(6, "a2")),
+        "$7" | "$a3" => Ok(Register::new(7, "a3")),
+        "$8" | "$t0" => Ok(Register::new(8, "t0")),
+        "$9" | "$t1" => Ok(Register::new(9, "t1")),
+        "$10" | "$t2" => Ok(Register::new(10, "t2")),
+        "$11" | "$t3" => Ok(Register::new(11, "t3")),
+        "$12" | "$t4" => Ok(Register::new(12, "t4")),
+        "$13" | "$t5" => Ok(Register::new(13, "t5")),
+        "$14" | "$t6" => Ok(Register::new(14, "t6")),
+        "$15" | "$t7" => Ok(Register::new(15, "t7")),
+        "$16" | "$s0" => Ok(Register::new(16, "s0")),
+        "$17" | "$s1" => Ok(Register::new(17, "s1")),
+        "$18" | "$s2" => Ok(Register::new(18, "s2")),
+        "$19" | "$s3" => Ok(Register::new(19, "s3")),
+        "$20" | "$s4" => Ok(Register::new(20, "s4")),
+        "$21" | "$s5" => Ok(Register::new(21, "s5")),
+        "$22" | "$s6" => Ok(Register::new(22, "s6")),
+        "$23" | "$s7" => Ok(Register::new(23, "s7")),
+        "$24" | "$t8" => Ok(Register::new(24, "t8")),
+        "$25" | "$t9" => Ok(Register::new(25, "t9")),
+        "$26" | "$k0" => Ok(Register::new(26, "k0")),
+        "$27" | "$k1" => Ok(Register::new(27, "k1")),
+        "$28" | "$gp" => Ok(Register::new(28, "gp")),
+        "$29" | "$sp" => Ok(Register::new(29, "sp")),
+        "$30" | "$fp" => Ok(Register::new(30, "fp")),
+        "$31" | "$ra" => Ok(Register::new(31, "ra")),
         _ => Err(anyhow!(
             "Unsupported register encounted: `{}` is not supported.",
             input
