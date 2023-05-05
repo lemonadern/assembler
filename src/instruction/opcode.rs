5ab96cd494f3a182c2d734222898c0af5ec22de8
lemonadern <taishi.develop@gmail.com>
2023-05-05 15:40:09 +0900

    feat: add tests for opcode

--- src/instruction/opcode.rs
+++ src/instruction/opcode.rs
@@ -15,7 +15,21 @@ impl Opcode {
     }
 
     pub fn to_binary_string(&self) -> String {
-        // nktk processor has 6-bit opcodes.
+        // This processor has 6-bit opcodes.
         binary_string(self.id as u64, 6)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn opcode_to_binary() {
+        let o = Opcode::new(0, "add");
+        assert_eq!(o.to_binary_string(), "000000");
+
+        let o = Opcode::new(7, "j");
+        assert_eq!(o.to_binary_string(), "000111");
     }
 }
